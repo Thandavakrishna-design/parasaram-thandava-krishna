f0042b9fea3392019bdd80ba86762cffb3af52f2
Thandavakrishna-design <53931630+Thandavakrishna-design@users.noreply.github.com>
2019-08-09 12:41:26 +0530

    Update coffeshop.c

--- coffeshop.c
+++ coffeshop.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-int main(void) {
+int main() {
     
-	// your code goes here
-	float amount,percent,value;
+	
+  float amount,percent,value;
   int sum;
   scanf("%f %f,&amount,&percent");
   sum=0;
@@ -12,6 +12,7 @@ int main(void) {
   sum=sum+amount;
   value=(percent/100)*amount;
   amount=amount-value;
-  printf("the discount rate is: ",amount");
+  printf("The amount necessary to order for free is: ",amount");
+  }
   return 0;
 }
